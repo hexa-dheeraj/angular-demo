e394f4bc13507e9e4f43398dd0435b1e00e3c2e7
Dheeraj Vats <dheeraj.vats@hexaviewtech.com>
2017-08-01 11:48:09 +0530

    Add ui grid

--- StudentApp/WebContent/app/dashboard/dashboardController.js
+++ StudentApp/WebContent/app/dashboard/dashboardController.js
@@ -22,20 +22,39 @@
 		 */
 		function initScope() {
 			_.extend($scope, {
-				studentList : new Array()
+				studentList : new Array(),
+				gridStudentInfo : {}
 			});
 
 			getStudentInfo();
+			initStudentInfoGrid();
 		}
 
 		function getStudentInfo() {
 			dashboardInfoService.getStudentInfo().success(
 					function(dataFromServer, status, headers, config) {
 						$scope.studentList = dataFromServer;
+						$scope.gridStudentInfo.data = dataFromServer;
 						console.log(dataFromServer);
 					}).error(function(dataFromServer, status, headers, config) {
 				console.log(dataFromServer);
 			});
+		}
+
+		function initStudentInfoGrid() {
+			$scope.gridStudentInfo.columnDefs = [ {
+				name : 'studentName',
+				displayName : 'Name'
+			}, {
+				name : 'studentId',
+				displayName : 'Id',
+			} , {
+				name : 'studentAge',
+				displayName : 'Age',
+			} , {
+				name : 'studentCourse',
+				displayName : 'Course Enrolled',
+			} ];
 		}
 
 	}
